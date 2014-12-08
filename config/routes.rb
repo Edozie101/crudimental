339089a79bbea2c86480deb3d7c88af3aad03b36
edzye101 <edzye101@gmail.com>
2014-12-08 15:07:01 -0800

    Added delete popup

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   get 'products/close'
 
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -24,7 +27,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :products
 
   # Example resource route with options:
   #   resources :products do
--- db/migrate/20141208175145_create_products.rb
+++ db/migrate/20141208175145_create_products.rb
@@ -2,8 +2,10 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.text_area :description
+      t.text :description
       t.integer :price
+      
+      t.timestamps
     end
   end
 end
